830a3b626c7cb5642df49631cdab595e65c77051
yesayaAndros <yesaya.alehandro@topremit.com>
2024-07-26 12:41:28 +0700

    feat: navigation bar

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,6 +1,11 @@
 import NavigationBar from "@/components/NavigationBar";
+import { Flex } from "@chakra-ui/react";
 
 const HomePage = () => {
-  return <NavigationBar />;
+  return (
+    <Flex w="100%" h="100vh" bgColor={"#EBEBEB"}>
+      <NavigationBar />
+    </Flex>
+  );
 };
 export default HomePage;
