ee2363341f9736808e93706ac3d00eddb00c4f51
yesayaAndros <yesaya.alehandro@topremit.com>
2024-07-27 12:55:29 +0700

    feat : add filter and category

---
+++ src/componentPage/FilterAndCategory/FilterAndCategoryWrapper.js
@@ -0,0 +1,103 @@
+"use client";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Flex,
+  List,
+  ListIcon,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { FaCircle } from "react-icons/fa";
+
+export const FilterAndCategoryWrapper = ({
+  options = [],
+  heading,
+  isOpen,
+  onToggle,
+  theme,
+  ...props
+}) => {
+  const [selectedFilter, setSelectedFilter] = useState("");
+
+  const isSelected = (id) => id === selectedFilter;
+  const isPrimaryTheme = theme === "primary";
+  return (
+    <Accordion
+      allowToggle
+      index={isOpen ? 0 : -1}
+      {...props}
+      maxH={"45%"}
+      w="100%"
+      bgColor={"white"}
+      borderRadius={"19px"}
+      py=".5em"
+      overflow={"hidden"}
+    >
+      <AccordionItem border={"none"} >
+        <Text fontWeight="bold">
+          <AccordionButton onClick={onToggle}>
+            {heading}
+            <AccordionIcon />
+          </AccordionButton>
+        </Text>
+        <Flex w="100%">
+          <AccordionPanel
+            overflowY={"auto"}
+            sx={{
+              "::-webkit-scrollbar": {
+                display: "none",
+              },
+              "-ms-overflow-style": "none", // IE and Edge
+              "scrollbar-width": "none", // Firefox
+            }}
+            maxH={"80%"}
+          >
+            <List>
+              {options.map((ele, idx) => {
+                const bgColor = isSelected(idx)
+                  ? isPrimaryTheme
+                    ? "primary"
+                    : "secondary"
+                  : "none";
+                const color = isSelected(idx)
+                  ? isPrimaryTheme
+                    ? "white"
+                    : "none"
+                  : "none";
+                return (
+                  <ListItem
+                    as={Flex}
+                    key={idx}
+                    gap=".20em"
+                    alignItems={"center"}
+                  >
+                    <ListIcon
+                      as={FaCircle}
+                      color={isPrimaryTheme ? "secondary" : "primary"}
+                    />
+                    <Flex
+                      w="100%"
+                      h="100%"
+                      p=".6em"
+                      bgColor={bgColor}
+                      color={color}
+                      fontWeight={isSelected(idx) ? "bold" : "regular"}
+                      onClick={() => setSelectedFilter(idx)}
+                    >
+                      {ele}
+                    </Flex>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </AccordionPanel>
+        </Flex>
+      </AccordionItem>
+    </Accordion>
+  );
+};
