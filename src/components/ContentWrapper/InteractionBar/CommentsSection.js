42dec3d90c592de9492898fb81695db2bb8ad009
yesayaAndros <yesaya.alehandro@topremit.com>
2024-09-12 13:13:30 +0700

    refactor: add comment interaction(reply and like)

--- src/components/ContentWrapper/InteractionBar/CommentsSection.js
+++ src/components/ContentWrapper/InteractionBar/CommentsSection.js
@@ -3,6 +3,7 @@ import { QUERY_KEY_CONSTANTS } from "@/constants/queryKey";
 import { usePostCommentMutations } from "@/hooks/queries/mutations/usePostCommentMutations";
 import { useCreateToast } from "@/hooks/useCreateToast";
 import { UserProfileIcon } from "@/icons/UserProfileIcon";
+import { colors } from "@/theme/foundations/colors";
 import { ConvertTimestampFormat } from "@/utils/timeStampConverter";
 import { Button, Flex, Text, Textarea } from "@chakra-ui/react";
 import { useQueryClient } from "@tanstack/react-query";
@@ -27,14 +28,70 @@ export const CommentsSection = ({
   });
 
   return (
-    <Flex direction="column" gap=".5em">
-      <Textarea
-        value={commentsValue}
-        onChange={(e) => setCommentsValue(e.target.value)}
-        placeholder="write your comment here"
-        size="sm"
-        borderRadius={"10px"}
-      />
+    <Flex direction="column" gap="1.5em">
+      <Flex direction={"column"} gap="1.25em">
+        <Text fontWeight="bold" p=".5em" borderBottom={"1px solid #E8E8E8"}>
+          Comments
+        </Text>
+        {commentsData?.map((ele, idx) => {
+          return (
+            <Flex key={idx} direction="column" gap=".5em" justify={"center"}>
+              <Flex gap=".5em" alignItems="center">
+                <UserProfileIcon />
+                <Flex gap=".2em" direction={"column"}>
+                  <Text fontWeight={"bold"} fontSize={"small"}>
+                    {ele.user.username}
+                  </Text>
+                  <Text fontSize={"small"} color={colors.myColor.black[400]}>
+                    {ConvertTimestampFormat(ele.created_at)}
+                  </Text>
+                </Flex>
+              </Flex>
+              <Flex
+                pl="2.5em"
+                w="100%"
+                pr="1em"
+                gap=".5em"
+                direction={"column"}
+              >
+                <Flex
+                  p="1em"
+                  w="100%"
+                  bgColor={colors.myColor.blue[100]}
+                  borderRadius={"10px"}
+                >
+                  <Text>{ele.content}</Text>
+                </Flex>
+                <Flex gap="1em">
+                  <Text
+                    as={"button"}
+                    fontSize={"small"}
+                    color={colors.myColor.black[400]}
+                  >
+                    Like
+                  </Text>
+                  <Text
+                    as={"button"}
+                    fontSize={"small"}
+                    color={colors.myColor.black[400]}
+                  >
+                    Reply
+                  </Text>
+                </Flex>
+              </Flex>
+            </Flex>
+          );
+        })}
+      </Flex>
+      <Flex gap=".5em" alignItems={"center"}>
+        <UserProfileIcon />
+        <Textarea
+          value={commentsValue}
+          onChange={(e) => setCommentsValue(e.target.value)}
+          placeholder="write your comment here"
+          borderRadius={"10px"}
+        />
+      </Flex>
       <Flex justifyContent={"end"}>
         <Button
           size={"sm"}
@@ -51,38 +108,6 @@ export const CommentsSection = ({
         >
           Post Comment
         </Button>
-      </Flex>
-      <Flex direction={"column"} gap="1.25em">
-        <Text fontWeight="bold" px=".5em">
-          Comments
-        </Text>
-        {commentsData?.map((ele, idx) => {
-          return (
-            <Flex key={idx} direction="column" gap=".5em" justify={"center"}>
-              <Flex gap=".5em" alignItems="center">
-                <UserProfileIcon />
-                <Flex gap=".2em" direction={"column"}>
-                  <Text fontWeight={"bold"} fontSize={"small"}>
-                    {ele.user.username}
-                  </Text>
-                  <Text fontSize={"small"}>
-                    {ConvertTimestampFormat(ele.created_at)}
-                  </Text>
-                </Flex>
-              </Flex>
-              <Flex pl="2.5em" w="100%" pr="1em">
-                <Flex
-                  p="1em"
-                  w="100%"
-                  bgColor={"#E4FAFF"}
-                  borderRadius={"10px"}
-                >
-                  <Text>{ele.content}</Text>
-                </Flex>
-              </Flex>
-            </Flex>
-          );
-        })}
       </Flex>
     </Flex>
   );
