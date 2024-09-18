29ac47c73563eb551d20a69b83444ff4f39318e4
yesayaAndros <yesaya.alehandro@topremit.com>
2024-09-18 13:41:26 +0700

    refactor: fetch and show profile image

--- src/icons/UserProfileIcon.js
+++ src/icons/UserProfileIcon.js
@@ -1,12 +1,23 @@
-"use client"
+"use client";
 import { colors } from "@/theme/foundations/colors";
-import { Circle, Icon } from "@chakra-ui/react";
+import { getUserInfo } from "@/utils/tokenHelper";
+import { Circle, Flex, Icon } from "@chakra-ui/react";
+import Image from "next/image";
 import { MdPerson } from "react-icons/md";
 
 export const UserProfileIcon = () => {
+  const {
+    userInfo: { url_img },
+  } = getUserInfo();
   return (
     <Circle size="35px" bg={colors.myColor.black[200]} color="white">
-      <Icon as={MdPerson} w="25px" h="25px" />
+      {url_img ? (
+        <Flex overflow={"hidden"} borderRadius={"full"}>
+          <Image width={35} height={35} src={url_img} alt="profile-picture" />
+        </Flex>
+      ) : (
+        <Icon as={MdPerson} w="25px" h="25px" />
+      )}
     </Circle>
   );
 };
